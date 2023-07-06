a8b49f0707f8ac2e27b8fe3f387af4e929d2f061
NishaMDev <89233753+NishaMDev@users.noreply.github.com>
2023-07-06 05:31:31 +0000

    added jinja code

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bangalore, India',
+    'salary': 'Rs 10,00,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Science',
+    'location': 'Bangalore, India',
+    'salary': 'Rs 20,00,000'
+  },
+  {
+    'id': 3,
+    'title': 'Backend Engineer',
+    'location': 'Remote',
+  },
+]
 
 
 @app.route("/")
 def hello_world():
-  return render_template("home.html")
+  return render_template("home.html", jobs=JOBS)
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=False)
